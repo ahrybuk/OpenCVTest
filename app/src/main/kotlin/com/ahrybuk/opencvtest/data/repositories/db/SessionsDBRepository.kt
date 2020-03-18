affb119d565d0bba81f0270ab5cf51bfb03d3461
Oleksandr Hrybuk <a.hrybuk@gmail.com>
2020-03-18 15:24:30 +0200

    add image util

--- app/src/main/kotlin/com/ahrybuk/opencvtest/data/repositories/db/SessionsDBRepository.kt
+++ app/src/main/kotlin/com/ahrybuk/opencvtest/data/repositories/db/SessionsDBRepository.kt
@@ -9,6 +9,7 @@ import com.ahrybuk.opencvtest.data.models.Session
 import com.ahrybuk.opencvtest.data.repositories.base.SessionRepository
 import com.ahrybuk.opencvtest.db.AppDatabase
 import com.ahrybuk.opencvtest.db.entities.SessionEntity
+import com.ahrybuk.opencvtest.utils.ImageUtil
 import io.reactivex.Observable
 import org.koin.core.KoinComponent
 import org.koin.core.inject
@@ -18,13 +19,13 @@ import java.io.*
 class SessionsDBRepository : SessionRepository, KoinComponent {
 
     private val appDatabase: AppDatabase by inject()
-    private val context: Context by inject()
+    private val imageUtil: ImageUtil by inject()
 
     override fun getSession(timeStamp: Long): Observable<Session> {
         return appDatabase.sessionDao().getSingleByTimestamp(timeStamp).map { entity ->
             Session(
-                pathToBitmap(entity.mainImagePath)!!,
-                pathToBitmap(entity.faceDetectedImagePath)!!,
+                imageUtil.loadBitmap(entity.mainImagePath)!!,
+                imageUtil.loadBitmap(entity.faceDetectedImagePath)!!,
                 entity.timestamp
             )
         }
@@ -35,8 +36,8 @@ class SessionsDBRepository : SessionRepository, KoinComponent {
             appDatabase.sessionDao().insert(
                 SessionEntity(
                     session.timestamp,
-                    bitmapToPath(session.mainImage, session.timestamp)!!,
-                    bitmapToPath(session.faceDetectedImage, session.timestamp)!!
+                    imageUtil.saveBitmap(session.mainImage, session.timestamp)!!,
+                    imageUtil.saveBitmap(session.faceDetectedImage, session.timestamp)!!
                 )
             ) > 0
         }
@@ -46,45 +47,11 @@ class SessionsDBRepository : SessionRepository, KoinComponent {
         return appDatabase.sessionDao().getAll().map {
             it.map { entity ->
                 Session(
-                    pathToBitmap(entity.mainImagePath)!!,
-                    pathToBitmap(entity.faceDetectedImagePath)!!,
+                    imageUtil.loadBitmap(entity.mainImagePath)!!,
+                    imageUtil.loadBitmap(entity.faceDetectedImagePath)!!,
                     entity.timestamp
                 )
             }
-        }
-    }
-
-    private fun bitmapToPath(bitmapImage: Bitmap, timeStamp: Long): String? {
-//        TODO check
-        val cw = ContextWrapper(context)
-        // path to /data/data/yourapp/app_data/imageDir
-        val directory = cw.getDir("images", MODE_PRIVATE)
-        // Create imageDir
-        val mypath = File(directory, "$timeStamp.jpg")
-        var fos: FileOutputStream? = null
-        try {
-            fos = FileOutputStream(mypath)
-            // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            try {
-                fos!!.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-        return directory.absolutePath
-    }
-
-    private fun pathToBitmap(path: String): Bitmap? {
-//        TODO check
-        try {
-            val f = File(path, "profile.jpg")
-            return BitmapFactory.decodeStream(FileInputStream(f))
-        } catch (e: FileNotFoundException) {
-            return null
         }
     }
 }
